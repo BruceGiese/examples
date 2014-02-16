95a5c5122aa716a636f391e206cb8a0c7aa6920e
BruceGiese <bruce.giese.home@gmail.com>
2014-02-16 10:52:54 -0500

    Added writing to the database.

--- MySqlTesting/src/foo/MySqlTesting.java
+++ MySqlTesting/src/foo/MySqlTesting.java
@@ -18,6 +18,7 @@ import java.sql.Statement;
  */
 
 public class MySqlTesting {
+	static final String TEXT_TO_ADD = "And they all lived happily ever after.";
 
 	public static void main(String[] args) {
 		Connection conn = null;
@@ -47,28 +48,50 @@ public class MySqlTesting {
             System.exit(-2);
         }
         
-        System.out.println("execute query to get all the sentences in the story");
+        
         try {
+        	int lastSentence = 0;	// Start with the initial value in case there are no sentences in the story yet.
+        	int rowCount = 0;
+        	String str;
+        	
         	stmt = conn.createStatement();
+
+        	System.out.println("execute query to get all the sentences in the story....");
         	rs = stmt.executeQuery("SELECT sentence from sentences WHERE accepted=1 ORDER BY sentence_number");
         	
-            System.out.println("display the query results...");
+            System.out.print("display the query results...\n    ");
         	if( rs.first() ) {
         		do {
         			// TODO send this out to a file rather than the console.
         			System.out.print(rs.getString("sentence") + " ");
         		} while( rs.next() );
-        		System.out.println("\nTo be continued");
         		
+        		// I could have done this above, but in the actual project I need to do it like this:
+        		System.out.println("\n\ndetermining the last sentence in the story...");
+        		rs.close();
+        		rs = stmt.executeQuery("SELECT MAX(sentence_number) from sentences");
+        		if( rs.first() ) {
+        			lastSentence = rs.getInt(1);	// There can be only one
+        			System.out.println("    The last sentence number is " + lastSentence);
+        		} else {
+        			throw new Exception("MySqlTesting: There was no maximum sentence_id");
+        		}
         	} else {
         		System.out.println("    There are no sentences in the story yet.");
         	}
+        	
+        	str = "INSERT INTO sentences (sentence,sentence_number,worker_id,votes,accepted) VALUES (\"" + TEXT_TO_ADD + "\"," + Integer.toString(lastSentence+1) + ",0,1,1)";
+        	System.out.println("updating the database using: " + str);
+        	rowCount = stmt.executeUpdate(str);
+        	
 
         } catch (SQLException qe) {
             System.out.println("    SQLException: " + qe.getMessage());
             System.out.println("    SQLState: " + qe.getSQLState());
             System.out.println("    VendorError: " + qe.getErrorCode());
         	System.exit(-3);
+        } catch (Exception e) {
+        	System.out.println(e);
         } finally {
         	if(rs != null) {
         		try {
